e81b70c3404157f222b3c0d0c6afc810e2d3a090
xiaofu <3473225549@qq.com>
2026-05-18 19:08:05 +0800

    Day4:学习了标识符，运算符，字符串的三定义和字符串的拼接

---
+++ Day4.py
@@ -0,0 +1,30 @@
+# 常见算术运算符
+a = 9
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+# 赋值运算符
+num = 9
+num += 1
+print("num += 1:", num)
+num -= 2
+print("num -= 2:", num)
+num *= 2
+print("num *= 2:", num)
+num /= 2
+print("num /= 2:", num)
+num %= 2
+print("num %= 2:", num)
+num **= 2
+print("num **= 2:", num)
+
+# 字符串的拼接
+name = "小紮"
+address = "不知名小城"
+print("我是" + name + ",我在" + address)
