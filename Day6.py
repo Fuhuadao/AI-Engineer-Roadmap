c054356f7f552932a1aa0011636a57f33fa1fcd8
xiaofu <3473225549@qq.com>
2026-05-20 20:10:02 +0800

    Day6:今天学习了input（）语句和布尔类型和比较运算符

---
+++ Day6.py
@@ -0,0 +1,22 @@
+# 运用input()语句输入
+print("你的名字是?")
+name = input()
+print(f"我知道了，你的名字是{name}")
+# 简化input（）的输入
+name = input("你的名字是：")
+print(f"我知道了，你的名字是{name}")
+
+# 定义变量名
+user_name = input("您好：")
+user_type = input("您是尊贵的：")
+print(f"您好{user_name},您是{user_type}用户，欢迎您的光临。") #输出结果
+
+# 比较运算符的使用
+num1 = 23
+num2 = 34
+print(f"num1 == num2的结果：{num1 == num2}")
+print(f"num1 != num2的结果：{num1 != num2}")
+print(f"num1 < num2的结果：{num1 < num2}")
+print(f"num1 > num2的结果：{num1 > num2}")
+print(f"num1 <= num2的结果：{num1 <= num2}")
+print(f"num1 >= num2的结果：{num1 >= num2}")
